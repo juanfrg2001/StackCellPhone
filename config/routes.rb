cbcd02b7b5aa77a819dbcc23e271bb33a711abdb
juanfrg2001 <juanfrg2001@gmail.com>
2022-10-01 13:08:47 -0500

    Adds index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+  root 'static_pages#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
